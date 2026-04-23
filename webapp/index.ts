3bdf1dbb948cd3b60eb807ba500cb80fb099aa17
lucastso10 <lucastso10@gmail.com>
2026-04-22 21:16:53 -0300

    started adding xml views

--- webapp/index.ts
+++ webapp/index.ts
@@ -1,1 +1,7 @@
-alert("This works!")
+import XMLView from "sap/ui/core/mvc/XMLView";
+
+XMLView.create({
+    viewName: "ui5.first.view.App"
+}).then(function (view) {
+    view.placeAt("content");
+});
