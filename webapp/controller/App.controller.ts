6dfa658c04d7c19770542af0d650e68124eccaa2
lucastso10 <lucastso10@gmail.com>
2026-04-24 20:53:03 -0300

    translated text

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,6 +1,8 @@
 import MessageToast from "sap/m/MessageToast";
 import Controller from "sap/ui/core/mvc/Controller"; 
 import JSONModel from "sap/ui/model/json/JSONModel";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
 
 /**
  * @name ui5.first.controller.App
@@ -14,9 +16,18 @@ export default class AppController extends Controller {
     };
     const dataModel = new JSONModel(data);
     this.getView()?.setModel(dataModel);
+
+    const i18nModel = new ResourceModel({
+      bundleName: "ui5.first.i18n.i18n"
+    });
+    this.getView()?.setModel(i18nModel, "i18n");
   }
 
   onButtonPress(): void {
-    MessageToast.show("Hello!");
+    const recipient = (this.getView()?.getModel() as JSONModel)?.getProperty("/recipient/name");
+    const resourceBundle = (this.getView()?.getModel("i18n") as ResourceModel)?.getResourceBundle() as ResourceBundle;
+    const msg = resourceBundle.getText("helloMsg", [recipient]) || "no text defined";
+
+    MessageToast.show(msg);
   }
 };
